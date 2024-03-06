8bc176eb29cbbcafc33920f0d61712e252cec9ce
Andy Holmes <andrew.g.r.holmes@gmail.com>
2024-03-06 09:19:12 -0800

    fix: better llvm analyzer summary

--- src/suites/analyzer.sh
+++ src/suites/analyzer.sh
@@ -37,22 +37,23 @@ actuary_suite_analyzer() {
         meson setup --buildtype=debug \
                     ${ACTUARY_SETUP_ARGS} \
                     "${ACTUARY_BUILDDIR}" && \
-        ninja -C "${ACTUARY_BUILDDIR}" scan-build 2> \
-            "${ACTUARY_BUILDDIR}/meson-logs/analyzer.log" || ANALYZER_ERROR=true
+        ninja -C "${ACTUARY_BUILDDIR}" scan-build > \
+            "${ACTUARY_BUILDDIR}/meson-logs/analyzer.log" || ANALYZER_ERROR=$?
 
-        if [ "${ANALYZER_ERROR}" = "true" ]; then
-            ANALYZER_OUTPUT=$(cat "${ACTUARY_BUILDDIR}/meson-logs/analyzer.log")
+        if [ "${ANALYZER_ERROR}" -ne 0 ]; then
+            echo "log=${ACTUARY_BUILDDIR}/meson-logs/scanbuild" >> "${GITHUB_OUTPUT}"
 
             if [ "${GITHUB_ACTIONS}" = "true" ]; then
                 echo "### LLVM Analyzer" >> "${GITHUB_STEP_SUMMARY}";
                 echo "\`\`\`c" >> "${GITHUB_STEP_SUMMARY}";
-                echo "${ANALYZER_OUTPUT}" >> "${GITHUB_STEP_SUMMARY}";
+                awk '/warning:/{print prev_line "\n" $0} {if ($0 ~ /^\[[0-9]+\/[0-9]+\]/) {prev_line = $0} else {prev_line = prev_line "\n" $0}}' \
+                    "${ACTUARY_BUILDDIR}/meson-logs/analyzer.log" >> \
+                    "${GITHUB_STEP_SUMMARY}";
                 echo "\`\`\`" >> "${GITHUB_STEP_SUMMARY}";
-
-                echo "log=${ACTUARY_BUILDDIR}/meson-logs/scanbuild" >> "${GITHUB_OUTPUT}"
             fi
 
-            echo "${ANALYZER_OUTPUT}" && exit 1;
+            cat "${ACTUARY_BUILDDIR}/meson-logs/analyzer.log"
+            return "${ANALYZER_ERROR}";
         fi
     fi
 }
