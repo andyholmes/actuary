c25972b06629e902820a27b119a4ad5295845f64
Andy Holmes <andrew.g.r.holmes@gmail.com>
2024-05-04 14:17:04 -0700

    refactor: compress actuary into a single script
    No one writes plugins.

--- src/actuary-scanbuild.sh
+++ src/actuary-scanbuild.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: No rights reserved
 
+# deadcode.DeadStores: allows assigning a value to an auto-cleanup
 scan-build --status-bugs \
            -disable-checker deadcode.DeadStores \
            "$@"
+
